1c4287fe87d353f8bc9b5062be92638aeb385b6b
CDN\xdz <845094708@qq.com>
2020-11-07 22:37:48 +0800

    fix to compile

--- include/utilities/memory.h
+++ include/utilities/memory.h
@@ -1,10 +1,11 @@
 #ifndef UTILITIES_MEMORY_H
 #define UTILITIES_MEMORY_H
 
+/*
 template<typename T, typename ...Args>
 std::unique_ptr<T> make_unique( Args&& ...args )
 {
     return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
 }
-
+*/
 #endif
